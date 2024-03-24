cde7959cfd7323f4de8169f0962c2f7c93e7c5a2
Supattra Saekow <64847261+bellsays@users.noreply.github.com>
2024-03-24 18:20:54 +0700

    Add files via upload

--- ass6-app-66130701722.py
+++ ass6-app-66130701722.py
@@ -18,7 +18,7 @@ def TextClean(text):
   return text
  
 # ตั้งชื่อหัวข้อหน้าเว็บ
-st.title("Text Sentiment Prediction Using Naive bayes Model Created By Supattra saekow ID: 6613070172")
+st.title("Text Sentiment Prediction Using Naive bayes Model Created By Supattra saekow ID: 66130701722")
  
 def main():
   # สร้าง Sidebar สำหรับรับข้อความ
@@ -35,7 +35,7 @@ def main():
  
   # แสดงผลลัพธ์
   if prediction == 'pos':
-    st.success("Positive")
+    st.success("`Positive")
   else:
     st.error("Negative")
  
