005816bcf8147fc1f28e73a68e22aa01f44f9a15
Supattra Saekow <64847261+bellsays@users.noreply.github.com>
2024-03-24 15:28:08 +0700

    Add files via upload

---
+++ ass6-app-66130701722.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pickle
+import re
+from pythainlp.util import normalize
+ 
+# โหลดโมเดล LogisticRegression
+model_bay = pickle.load(open('naive_bayes_model.sav', 'rb'))
+ 
+# โหลด TfidfVectorizer
+tfidf_vectorizer = pickle.load(open('tfidf_vectorizer.sav', 'rb'))
+ 
+# ฟังก์ชันทำความสะอาดข้อความ
+def TextClean(text):
+  text = re.sub('<[^>]*>', '', text)
+  emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+  text = (re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', ''))
+  text = normalize(text)
+  return text
+ 
+# ตั้งชื่อหัวข้อหน้าเว็บ
+st.title("Text Sentiment Prediction Using Logistic Regression Model Created By Siwayu Seeyangnok ID: 66130701712")
+ 
+def main():
+  # สร้าง Sidebar สำหรับรับข้อความ
+  text = st.text_input("ป้อนข้อความของคุณ")
+ 
+  # ทำความสะอาดข้อความ
+  text = TextClean(text)
+ 
+  # แปลงข้อความให้เป็นเวกเตอร์ TF-IDF
+  X_new_tfidf = tfidf_vectorizer.transform([text])
+ 
+  # ทำนายผล
+  prediction = model_bay.predict(X_new_tfidf)
+ 
+  # แสดงผลลัพธ์
+  if prediction == 'pos':
+    st.success("`Positive")
+  else:
+    st.error("Negative")
+ 
+if __name__ == "__main__":
+  main()
